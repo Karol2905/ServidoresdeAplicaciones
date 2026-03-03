4732be63be25799e39cd3032a44f9380f7a1f42a
unknown <karol.estupinan-v@labinfo.is.escuelaing.edu.co>
2026-03-03 18:29:34 -0500

    Primer commit: proyecto MicroSpringBoot

---
+++ src/main/java/org/example/MicroSpringBoot.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MicroSpringBoot {
+    public static Map<String,Method> controllerMethods = new HashMap<>();
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+        System.out.println("Loading rest controllers and their methods... ");
+
+        Class c = Class.forName(args[0]);
+
+        if (c.isAnnotationPresent(RestController.class)){
+            for (Method m:c.getDeclaredMethods()){
+                if (m.isAnnotationPresent(GetMapping.class)){
+                    GetMapping a= m.getAnnotation(GetMapping.class);
+                    String path = a.value();
+                    controllerMethods.put(path,m);
+                }
+            }
+        }
+
+        System.out.println("Running method controller for  path: "+args[1]);
+
+        Method m = controllerMethods.get(args[1]);
+
+        System.out.println(m.invoke(null));
+    }
+}
